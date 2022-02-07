7aa87685a1f5317ec445545e1b39a05d00c45a61
jespiritu98 <jespiritu98@gmail.com>
2022-02-07 09:44:41 -0700

    Created Category class model with CategoryID and CategoryName, and Added ToDoResponse Model with Task, DueDate, Quadrant, and Completed.

---
+++ Models/ToDoResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6GroupAssignment.Models
+{
+    public class ToDoResponse
+    {
+        [Key]
+        [Required]
+        public int ToDoListId { get; set; }
+
+        [Required(ErrorMessage = "Can't watch a movie without knowing the title...")]
+        public string Task { get; set; }
+       
+        public string DueDate { get; set; }
+        
+        [Required(ErrorMessage = "Which Quadrant is it in?")]
+        public int Quadrant { get; set; }
+        
+        public string Completed { get; set; }
+
+     
+        //Foreign Key Relationship
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
